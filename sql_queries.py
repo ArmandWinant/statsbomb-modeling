be71ba6302826d5325b6fa144bc62aa25fc49e81
Bastien Winant <babw1@student.london.ac.uk>
2022-08-22 22:07:09 +0200

    separate competitions from match table

--- sql_queries.py
+++ sql_queries.py
@@ -6,6 +6,7 @@ players_table_drop = "DROP TABLE IF EXISTS players;"
 teams_table_drop = "DROP TABLE IF EXISTS teams;"
 competitions_table_drop = "DROP TABLE IF EXISTS competitions;"
 related_events_table_drop = "DROP TABLE IF EXISTS related_events;"
+referees_table_drop = "DROP TABLE IF EXISTS referees";
 
 # CREATE TABLES
 
@@ -31,13 +32,19 @@ events_table_create = ("""
 matches_table_create = ("""
     CREATE TABLE IF NOT EXISTS matches (
         id SMALLINT PRIMARY KEY,
-        stadium SMALLINT,
         match_date DATE,
+        kick_off TIME,
         competition SMALLINT,
+        season CHAR(9),
+        match_week SMALLINT,
+        competition_stage VARCHAR,
+        stadium SMALLINT,
         home_team SMALLINT,
         away_team SMALLINT,
         home_score SMALLINT,
-        away_score SMALLINT
+        away_score SMALLINT,
+        match_status VARCHAR,
+        last_updated TIMESTAMP
     );
 """)
 
@@ -75,6 +82,14 @@ related_events_table_create = ("""
     );
 """)
 
+referees_table_create = ("""
+    CREATE TABLE IF NOT EXISTS referees (
+        id SMALLINT PRIMARY KEY,
+        name VARCHAR,
+        country SMALLINT
+    );
+""")
+
 # INSERT RECORDS
 
 events_table_insert = ("""
@@ -102,17 +117,29 @@ events_table_insert = ("""
 matches_table_insert = ("""
     INSERT INTO matches (
         id,
-        stadium,
         match_date,
+        kick_off,
         competition,
+        season,
+        match_week,
+        competition_stage,
+        stadium,
         home_team,
         away_team,
         home_score,
-        away_score
+        away_score,
+        match_status,
+        last_updated
     )
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     ON CONFLICT (id)
-    DO NOTHING;
+    DO UPDATE
+    SET
+        match_status = EXCLUDED.match_status,
+        kick_off = EXCLUDED.kick_off,
+        home_score = EXCLUDED.home_score,
+        away_score = EXCLUDED.away_score,
+        last_updated = EXCLUDED.last_updated;
 """)
 
 players_table_insert = ("""
@@ -163,6 +190,17 @@ related_events_table_insert = ("""
     VALUES (%s, %s);
 """)
 
+referees_table_insert = ("""
+    INSERT INTO referees (
+        id,
+        name,
+        country
+    )
+    VALUES (%s, %s, %s)
+    ON CONFLICT (id)
+    DO NOTHING;
+""")
+
 # FIND SONGS
 
 song_select = ("""
@@ -171,6 +209,6 @@ song_select = ("""
 # QUERY LISTS
 
 create_table_queries = [events_table_create, matches_table_create, players_table_create, teams_table_create,
-                        competitions_table_create, related_events_table_create]
+                        competitions_table_create, related_events_table_create, referees_table_create]
 drop_table_queries = [events_table_drop, matches_table_drop, players_table_drop, teams_table_drop, competitions_table_drop,
-                      related_events_table_drop]
+                      related_events_table_drop, referees_table_drop]
